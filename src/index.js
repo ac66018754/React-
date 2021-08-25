f1d516530986d6bd68a9cc697786cd1b91193ce8
Bruce <a0987461866@gmail.com>
2021-08-25 19:50:09 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import T from './Todolist';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <T/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
